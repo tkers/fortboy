f0ea01e46dd6fb095a98412e1c09e23e8d467447
Tijn Kersjes <tijn@divbyzero.nl>
2022-03-08 22:18:08 +0100

    Add pager to .wrapped

--- wrap.fs
+++ wrap.fs
@@ -1,7 +1,11 @@
 \ wrap text
 
 20 constant max-line
+18 constant max-height
+variable lineno
+
 : .wrapped ( buf len -- )
+  0 lineno !
   begin
     dup max-line >
   while
@@ -9,6 +13,10 @@
     begin 1- 2dup + c@ bl = until
     dup 1+ >r
     begin 1- 2dup + c@ bl <> until
+    lineno @ max-height = if
+      0 lineno ! key drop page
+    then
+    1 lineno +!
     1+ type cr
     r> /string
   repeat type cr ;
@@ -19,7 +27,7 @@
   loop ;
 
 : main
-  s\" WEST OF HOUSE\nYou are standing in an open field west of a white house, with a boarded front door.\nThere is a small mailbox here."
+  s\" You are in the kitchen of the white house. A table seems to have been used recently for the preparation of food. A passage leads to the west and a dark staircase can be seen leading upward. A dark chimney leads down and to the east is a small window which is open.\nOn the table is an elongated brown sack, smelling of hot peppers.\nA bottle is sitting on the table.\nThe glass bottle contains:\nA quantity of water"
   2dup strip-nl
   .wrapped
   bye ;

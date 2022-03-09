3c1a72255d7a79165c56a307eb43294213b7ace9
Tijn Kersjes <tijn@divbyzero.nl>
2022-03-09 23:30:13 +0100

    Disable randomness for testing

--- game.fs
+++ game.fs
@@ -12,10 +12,10 @@ require ./maze.fs
   install-font
   init-term
   init-input
-  w" Press any key to start!"
-  key drop page
-  utime seed !
-  \ 1234 seed !
+  \ w" Press any key to start!"
+  \ key drop page
+  \ utime seed !
+  1234 seed !
   make-and-show
   key drop page
   w" You are in the kitchen of the white house. A table seems to have been used recently for the preparation of food. A passage leads to the west and a dark staircase can be seen leading upward. A dark chimney leads down and to the east is a small window which is open. On the table is an elongated brown sack, smelling of hot peppers. A bottle is sitting on the table. The glass bottle contains: A quantity of water"

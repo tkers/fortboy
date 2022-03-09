a771c1ba26146fe45c119580be9a561aad4d5d12
Tijn Kersjes <tijn@divbyzero.nl>
2022-03-09 19:34:22 +0100

    Add structs for rooms

--- maze.fs
+++ maze.fs
@@ -1,3 +1,5 @@
+require struct.fs
+
 \ utils
 :m square dup * ;
 
@@ -5,11 +7,28 @@
 10 constant #rooms
 #rooms square constant #grid
 
+ROM
+struct
+  cell field room-grid
+  cell field room-index
+  cell field room-name
+  cell field room-description
+  cell field room-north
+  cell field room-east
+  cell field room-south
+  cell field room-west
+end-struct room%
+RAM
+
+: rooms room% * ;
+:m rooms room% * ;
+
+
 \ occupancy grid to check free space
 CREATE grid #grid allot
 
-: occupy 1 swap c! ;
-: is-occupied? c@ 1 = ;
+: occupy c! ;
+: is-occupied? c@ 0 <> ;
 
 : xy>grid ( x y -- addr )
   #rooms * + grid + ;
@@ -28,14 +47,32 @@ CREATE grid #grid allot
 
 \ list with push and peek
 variable roomlist-length
-create roomlist #rooms cells allot
+create roomlist #rooms rooms allot
 
-: push ( x -- )
-  roomlist roomlist-length @ cells + !
+: ix>name
+  case
+     1 of s" one"   endof
+     2 of s" two"   endof
+     3 of s" three" endof
+     4 of s" four"  endof
+     5 of s" five"  endof
+     6 of s" six"   endof
+     7 of s" seven" endof
+     8 of s" eight" endof
+     9 of s" nine"  endof
+    10 of s" ten"   endof
+  endcase ;
+
+: push ( xy i -- )
+  roomlist roomlist-length @ rooms + >r
+  dup r@ room-index !
+  ix>name r@ room-name 2!
+  r@ room-grid !
+  r> drop
   1 roomlist-length +! ;
 
-: peek ( u -- x )
-  cells roomlist + @ ;
+: seek ( u -- x )
+  rooms roomlist + ;
 
 \ room utils
 : rand-grid ( -- addr )
@@ -55,20 +92,19 @@ create roomlist #rooms cells allot
 variable current-room
 : make-and-show
   0 roomlist-length !
-  grid #grid erase
+  grid #grid 0 fill
 
   \ first room
   rand-grid
-  dup occupy
-  dup push
-  current-room !
+  ( roomlist-length @ 1+ ) 1 over occupy
+  1 push
 
   #rooms 1 do
-    roomlist-length @ random peek \ addr
+    roomlist-length @ random seek room-grid @ \ addr
     4 random nesw  \ addr2
     dup is-free? if
-      dup occupy
-      push
+      I 1+ over occupy
+      I 1+ push
       1
     else
       drop
@@ -94,22 +130,25 @@ variable current-room
 
   key drop
 
+  0 current-room !
+
   begin
     page
 
-    current-room @
+    current-room @ seek
 
-    ." ~~ in room " dup . ." ~~" cr
+    ." ~~ in room " dup room-name 2@ type space ." ~~" cr
     cr
     ." Doors:" cr
-    dup 0 nesw is-occupied? if ." - North" cr then
-    dup 1 nesw is-occupied? if ." - East" cr then
-    dup 2 nesw is-occupied? if ." - South" cr then
-    dup 3 nesw is-occupied? if ." - West" cr then
+    dup @ 0 nesw is-occupied? if ." - North" cr then
+    dup @ 1 nesw is-occupied? if ." - East" cr then
+    dup @ 2 nesw is-occupied? if ." - South" cr then
+    dup @ 3 nesw is-occupied? if ." - West" cr then
     cr
     ."  Where to next?" cr
     ." (use arrow keys)" cr
 
+    room-grid @
     key case
       k-up    of 0 endof
       k-right of 1 endof
@@ -118,6 +157,6 @@ variable current-room
                  4
     endcase nesw
 
-    dup is-occupied? if current-room ! page else drop then
+    dup is-occupied? if c@ 1- current-room ! page else drop then
   again
  ;

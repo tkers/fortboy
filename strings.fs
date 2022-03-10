721bb8b9fcea265f7a004f5a043a3ea9ddce5208
Tijn Kersjes <tijn@divbyzero.nl>
2022-03-10 18:52:57 +0100

    Suport longer counted strings

--- strings.fs
+++ strings.fs
@@ -4,17 +4,17 @@
 create pad pad-size allot
 
 : place ( c-addr1 u1 c-addr2 -- )
-  over >r rot over char+ r> move c! ;
+  over >r rot over cell+ r> move ! ;
 
 : count ( c-addr1 -- c-addr2 u )
-  dup char+ swap c@ ;
+  dup cell+ swap @ ;
 
 : append ( c-addr1 u1 c-addr2 -- )
   over over >r >r
   count chars +
   swap chars move
-  r> r@ c@ + r> c! ;
+  r> r@ @ + r> ! ;
 
 : cappend ( c c-addr )
-  dup c@ char+ over c!
-  count 1- chars + c! ;
+  dup @ char+ over !
+  count 1- chars + ! ;

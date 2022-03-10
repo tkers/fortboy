e70c6e072ae38f48c2bd9ff9d1306fad543f3ae9
Tijn Kersjes <tijn@divbyzero.nl>
2022-03-10 18:24:47 +0100

    Simplify word wrap

--- wrap.fs
+++ wrap.fs
@@ -1,24 +1,22 @@
 require compat.fs
+require gbhw.fs
 
-20 constant max-line
-18 constant max-height
-variable lineno
+SCRN_X_B constant max-width
+SCRN_Y_B constant max-height
 
 : .wrapped ( buf len -- )
-  0 lineno !
   begin
-    dup max-line >
+    dup max-width >
   while
-    over max-line
+    over max-width
     begin 1- 2dup + c@ bl = until
     dup 1+ >r
     begin 1- 2dup + c@ bl <> until
-    lineno @ max-height = if
-      0 lineno ! key drop page
-    then
-    1 lineno +!
     1+ type cr
     r> /string
+    cursor-y @ max-height = if
+      key drop page
+    then
   repeat type cr ;
 
 :m w" postpone s" postpone .wrapped ; immediate

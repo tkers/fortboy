3d58c6534f0aeaae8bd80c9e1c6131d6d80c5f63
Tijn Kersjes <tijn@divbyzero.nl>
2022-03-12 15:47:37 +0100

    Store popup frame in ROM

--- popup.fs
+++ popup.fs
@@ -1,3 +1,21 @@
+ROM
+create bl-bl-3 bl c, bl c, 3 c,
+here create popup-frame
+bl c, bl c,  5 c,  1 c,  1 c,  1 c,  1 c,  1 c,  1 c,  1 c,  1 c,  1 c,  1 c,  1 c,  1 c,  1 c,  1 c,  6 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,
+bl c, bl c,  3 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,  4 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,
+bl c, bl c,  3 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,  4 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,
+bl c, bl c,  3 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,  4 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,
+bl c, bl c,  3 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,  4 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,
+bl c, bl c,  3 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,  4 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,
+bl c, bl c,  3 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,  4 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,
+bl c, bl c,  3 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,  4 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,
+bl c, bl c,  3 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,  4 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,
+bl c, bl c,  3 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,  4 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,
+bl c, bl c,  3 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,  4 c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c, bl c,
+bl c, bl c,  7 c,  2 c,  2 c,  2 c,  2 c,  2 c,  2 c,  2 c,  2 c,  2 c,  2 c,  2 c,  2 c,  2 c,  2 c,  8 c,
+here swap - constant popup-frame-size
+RAM
+
 : .tiny ( buf len -- )
   begin
     dup 14 >
@@ -6,26 +24,11 @@
     begin 1- 2dup + c@ bl = until
     dup 1+ >r
     begin 1- 2dup + c@ bl <> until
-    1+ type cr space space 3 emit
+    1+ type cr bl-bl-3 3 type
     r> /string
   repeat type ;
 
-create popup-pad 1024 chars allot
-
 : popup
-  0 popup-pad !
-  bl popup-pad cappend   bl popup-pad cappend 5 popup-pad cappend 14 0 do 1 popup-pad cappend loop 6 popup-pad cappend   14 0 do bl popup-pad cappend loop
-  bl popup-pad cappend   bl popup-pad cappend 3 popup-pad cappend s"               " popup-pad append      4 popup-pad cappend   14 0 do bl popup-pad cappend loop
-  bl popup-pad cappend   bl popup-pad cappend  3 popup-pad cappend s"               " popup-pad append      4 popup-pad cappend   14 0 do bl popup-pad cappend loop
-  bl popup-pad cappend   bl popup-pad cappend  3 popup-pad cappend s"               " popup-pad append      4 popup-pad cappend   14 0 do bl popup-pad cappend loop
-  bl popup-pad cappend   bl popup-pad cappend  3 popup-pad cappend s"               " popup-pad append      4 popup-pad cappend   14 0 do bl popup-pad cappend loop
-  bl popup-pad cappend   bl popup-pad cappend  3 popup-pad cappend s"               " popup-pad append      4 popup-pad cappend   14 0 do bl popup-pad cappend loop
-  bl popup-pad cappend   bl popup-pad cappend  3 popup-pad cappend s"               " popup-pad append      4 popup-pad cappend   14 0 do bl popup-pad cappend loop
-  bl popup-pad cappend   bl popup-pad cappend  3 popup-pad cappend s"               " popup-pad append      4 popup-pad cappend   14 0 do bl popup-pad cappend loop
-  bl popup-pad cappend   bl popup-pad cappend  3 popup-pad cappend s"               " popup-pad append      4 popup-pad cappend   14 0 do bl popup-pad cappend loop
-  bl popup-pad cappend   bl popup-pad cappend  3 popup-pad cappend s"               " popup-pad append      4 popup-pad cappend   14 0 do bl popup-pad cappend loop
-  bl popup-pad cappend   bl popup-pad cappend  3 popup-pad cappend s"               " popup-pad append      4 popup-pad cappend   14 0 do bl popup-pad cappend loop
-  bl popup-pad cappend   bl popup-pad cappend 7 popup-pad cappend 14 0 do 2 popup-pad cappend loop 8 popup-pad cappend
-  popup-pad count
+  popup-frame popup-frame-size
   0 3 at-xy type
   3 4 at-xy .tiny ;

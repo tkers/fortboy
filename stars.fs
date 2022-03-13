321cee34e0a85722d5c44b203d330f046f7f0edd
Tijn Kersjes <tijn@divbyzero.nl>
2022-03-13 21:46:56 +0100

    Cleanup font and add some stars

--- stars.fs
+++ stars.fs
@@ -2,16 +2,16 @@ rom
 here create stars
 
 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
-11 c, 11 c, 11 c, 11 c, 24 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 16 c, 11 c, 11 c, 11 c, 11 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
+11 c, 11 c, 11 c, 11 c, 12 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 13 c, 11 c, 11 c, 11 c, 11 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
 
-11 c, 11 c, 18 c, 17 c, 11 c, 18 c, 17 c, 11 c, 18 c, 17 c, 24 c, 11 c, 11 c, 11 c, 11 c, 11 c, 24 c, 11 c, 11 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
-16 c, 11 c, 23 c, 22 c, 11 c, 23 c, 22 c, 11 c, 23 c, 22 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 16 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
-11 c, 11 c, 23 c, 19 c, 21 c, 20 c, 19 c, 21 c, 20 c, 22 c, 11 c, 11 c, 24 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
+11 c, 11 c, 18 c, 17 c, 11 c, 18 c, 17 c, 11 c, 18 c, 17 c, 12 c, 11 c, 11 c, 11 c, 11 c, 11 c, 12 c, 11 c, 11 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
+14 c, 11 c, 23 c, 22 c, 11 c, 23 c, 22 c, 11 c, 23 c, 22 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 14 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
+11 c, 11 c, 23 c, 19 c, 21 c, 20 c, 19 c, 21 c, 20 c, 22 c, 11 c, 11 c, 12 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
 
-11 c, 11 c, 23 c, bl c, bl c, bl c, bl c, bl c, bl c, 22 c, 11 c, ( title ) 25 c, 26 c, 27 c, 28 c, 11 c, 29 c, 30 c, 31 c, ( 24 c, 11 c, 11 c, 11 c, 24 c, 11 c, 11 c, 11 c, ) 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
-11 c, 24 c, 23 c, bl c, bl c, bl c, bl c, bl c, bl c, 22 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
-11 c, 11 c, 23 c, bl c, bl c, bl c, bl c, bl c, bl c, 22 c, 16 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 24 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
-16 c, 11 c, 23 c, bl c, bl c, bl c, bl c, bl c, bl c, 22 c, 11 c, 11 c, 11 c, 24 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
+11 c, 11 c, 23 c, bl c, bl c, bl c, bl c, bl c, bl c, 22 c, 11 c, ( title ) 24 c, 25 c, 26 c, 27 c, 11 c, 28 c, 29 c, 30 c, ( 12 c, 11 c, 11 c, 11 c, 12 c, 11 c, 11 c, 11 c, ) 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
+11 c, 12 c, 23 c, bl c, bl c, bl c, bl c, bl c, bl c, 22 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
+11 c, 11 c, 23 c, bl c, bl c, bl c, bl c, bl c, bl c, 22 c, 13 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 12 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
+13 c, 11 c, 23 c, bl c, bl c, bl c, bl c, bl c, bl c, 22 c, 11 c, 11 c, 11 c, 12 c, 11 c, 11 c, 11 c, 11 c, 11 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
 
 11 c, 11 c, 23 c, bl c, bl c, bl c, bl c, bl c, bl c, 22 c, 11 c, 18 c, 17 c, 11 c, 18 c, 17 c, 11 c, 18 c, 17 c, 11 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,
 21 c, 21 c, 20 c, bl c, bl c, bl c, bl c, bl c, bl c, 19 c, 21 c, 20 c, 19 c, 21 c, 20 c, 19 c, 21 c, 20 c, 19 c, 21 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c, 0 c,

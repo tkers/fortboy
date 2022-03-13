428762cbf5b344918b22c45aae5b10ce540bd02e
Tijn Kersjes <tijn@divbyzero.nl>
2022-03-14 00:27:56 +0100

    Add outro

--- outro.fs
+++ outro.fs
@@ -1,6 +1,27 @@
 : show-outro
-  0 8 at-xy
-  ."   Hooray, you win!" cr
-  ."    Gold found: " gold-coins ?
+  page
+  s" Outside" center type cr
+  =====
+  s" You squint your eyes at the sudden change in brightness. You're back outside! The adventure " pad place
+  gold-coins @ ?dup 0= if
+    s" behind you is " pad append
+  else
+    s" is behind you, " pad append
+    dup 1 = if
+      drop
+      s" and the gold coin in your pocket is " pad append
+    else
+      s" and the " pad append
+      pad #append
+      bl pad cappend
+      s" gold coins in your pocket are " pad append
+    then
+  then
+  s" the only proof that any of it was real in the first place. Or was it? The memories are already starting to fade..." pad append
+  pad count .wrapped
+  key drop
+  page
+  2 8 at-xy ." You made it out!"
+  3 9 at-xy ." Gold coins: " gold-coins ?
   snd-hooray
   key drop ;

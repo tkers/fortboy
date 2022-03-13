4288eb77ce98e5e4abd482ecf34e54f9b6cf9bc4
Tijn Kersjes <tijn@divbyzero.nl>
2022-03-14 00:49:28 +0100

    Update intro

--- intro.fs
+++ intro.fs
@@ -1,11 +1,27 @@
 require ./hero.fs
 
+: month
+  12 random case
+    0 of s" January" endof
+    1 of s" February" endof
+    2 of s" March" endof
+    3 of s" April" endof
+    4 of s" May" endof
+    5 of s" June" endof
+    6 of s" July" endof
+    7 of s" August" endof
+    8 of s" September" endof
+    9 of s" October" endof
+   10 of s" November" endof
+   11 of s" December" endof
+  endcase ;
+
 : state-of-being
   6 random case
     0 of s" bounding headache" endof
     1 of s" deep frown" endof
     2 of s" soft gasp" endof
-    3 of s" big jawn" endof
+    3 of s" big yawn" endof
     4 of s" small shiver" endof
     5 of s" flinch" endof
   endcase ;
@@ -32,25 +48,36 @@ endcase ;
   endcase ;
 
 : show-intro
+  page
+  \ date header
+  0 pad !
+  30 random 1+ pad #append
+  bl pad cappend
+  month pad append
+  bl pad cappend
+  667 random 1203 + pad #append
+  pad count center type cr
+  =====
+
   s" You wake up with a " pad place
   state-of-being pad append
-  s"  from a " pad append
+  s"  in a " pad append
   atmosphere pad append
-  s"  room. Your " pad append
+  s"  fortress. Your " pad append
   state-of-mind pad append
-  s"  and the only thing you can remember is your name, " pad append
+  s" , and the only thing you can remember is your name: " pad append
   first-name pad append
   bl pad cappend
   last-name-1 pad append
   last-name-2 pad append
   [char] . pad cappend
+  bl pad cappend
 
-  page pad count .wrapped
-  key drop
+  pad count .wrapped
+  key drop page
 
-  s" Yet for a reason you can't explain, none of this makes you feel scared. For ahead of you an adventure awaits, and aspiring adventurers (or professional " pad place
+  s" Being a simple " pad place
   profession pad append
-  s" ) have no time to waste!" pad append
-
-  page pad count .wrapped
+  s" , you have no reason to be in this strange place. Better take a look around and unravel this mystery. Be brave, unlikely hero. Adventure awaits!" pad append
+  pad count .wrapped
   key drop ;

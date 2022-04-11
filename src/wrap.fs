11853496ce736d17e30ab48df4404dd87300296a
Tijn Kersjes <tijn@divbyzero.nl>
2022-04-11 22:29:01 +0200

    Fix .WRAPPED not using full width

--- src/wrap.fs
+++ src/wrap.fs
@@ -7,11 +7,10 @@ SCRN_Y_B constant max-height
   begin
     dup max-width >
   while
-    over max-width
+    over max-width 1+
     begin 1- 2dup + c@ bl = until
     dup 1+ >r
-    begin 1- 2dup + c@ bl <> until
-    1+ type cr
+    type cr
     r> /string
     cursor-y @ max-height = if
       key drop partial-page

de636c85c682519bc9d9bd7a5b5d1e1af7677664
Tijn Kersjes <tijn@divbyzero.nl>
2022-12-20 20:00:10 +0100

    Update hero name generator

--- src/hero.fs
+++ src/hero.fs
@@ -1,74 +1,116 @@
 ROM
-create vowels
-emit-bytes( 'a 'e 'i 'o 'u )
-
-create consonants
-emit-bytes( 'b 'd 'f 'g 'k 'l 'm 'n 'p 'r 's 't 'v 'z )
-\ only for first consonant
-emit-bytes( 'c 'h 'j 'q 'w 'x 'y )
-RAM
-
-: random-vowel 5 random vowels + c@ ;
-: random-consonant 21 random consonants + c@ ;
-: random-consonant-snd 14 random consonants + c@ ;
-: to-upper 32 - ;
-
-create fname 5 chars cell+ allot
-: first-name
-  0 fname !
-  random-consonant to-upper fname cappend
-  random-vowel              fname cappend
-  random-consonant-snd dup  fname cappend fname cappend
-  random-vowel              fname cappend
-  fname count ;
-
-ROM
 
 start-strings
-  -" Dragon"
-  -" Truth"
-  -" Light"
-  -" Oath"
-  -" Shield"
-  -" Sword"
-  -" Fire"
+  -" Adriana"
+  -" Aurelia"
+  -" Basil"
+  -" Cassian"
+  -" Celeste"
+  -" Dante"
+  -" Emery"
+  -" Finley"
+  -" Gavin"
+  -" Harper"
+  -" Idalia"
+  -" Jamie"
+  -" Kazamir"
+  -" Lucien"
+  -" Martin"
+  -" Niamh"
+  -" Oliver"
+  -" Pascal"
+  -" Quinn"
+  -" Revna"
+  -" Riley"
+  -" Sienna"
+  -" Silas"
+  -" Tali"
+  -" Ulysses"
+  -" Viola"
+  -" Wyatt"
+  -" Xavier"
+  -" Yara"
+  -" Yasmin"
+  -" Zelda"
+  -" Zora"
+end-strings: first-name[]
+: first-name 32 random first-name[] ;
+
+start-strings
+  -" Arrow"
+  -" Bone"
   -" Cloud"
+  -" Crow"
   -" Death"
-  -" Grave"
+  -" Dragon"
+  -" Dirt"
   -" Fear"
-  -" Fury"
+  -" Flame"
+  -" Flint"
+  -" Grave"
+  -" Gold"
+  -" Hope"
+  -" Key"
+  -" Light"
+  -" Moon"
+  -" Myth"
+  -" Oath"
+  -" Quest"
+  -" Rock"
+  -" Shade"
+  -" Shield"
+  -" Snow"
+  -" Sword"
+  -" Tree"
+  -" Truth"
+  -" Vale"
+  -" Wood"
 end-strings: last-name-1[]
 
-: last-name-1 12 random last-name-1[] ;
+: last-name-1 28 random last-name-1[] ;
 
 start-strings
-  -" bringer"
-  -" smasher"
   -" breaker"
-  -" seer"
-  -" wielder"
-  -" breather"
-  -" speaker"
-  -" teller"
-  -" whisperer"
+  -" bringer"
+  -" crusher"
+  -" dancer"
+  -" dodger"
+  -" finder"
   -" gazer"
+  -" hoarder"
+  -" hugger"
+  -" juggler"
+  -" keeper"
+  -" napper"
+  -" packer"
+  -" razer"
+  -" seeker"
+  -" slinger"
+  -" taker"
+  -" teller"
+  -" voider"
+  -" wielder"
 end-strings: last-name-2[]
 
-: last-name-2 10 random last-name-2[] ;
+: last-name-2 20 random last-name-2[] ;
 
 start-strings
-  -" shepard"
-  -" blacksmith"
   -" alchemist"
-  -" druid"
-  -" soothsayer"
-  -" herbologist"
-  -" goldminer"
-  -" sharpshooter"
-  -" warlock"
+  -" baker"
+  -" blacksmith"
   -" cleric"
-  -" paladin"
-  -" ranger"
+  -" farmer"
+  -" goldminer"
+  -" herbologist"
+  -" innkeeper"
+  -" merchant"
+  -" minstrel"
+  -" peasant"
+  -" shepard"
+  -" soothsayer"
+  -" tailor"
+  -" thief"
+  -" weaver"
 end-strings: profession[]
 
-: profession 12 random profession[] ;
+: profession 16 random profession[] ;
